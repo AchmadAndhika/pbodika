c63e494e88b86992af398172765cbd4f009c9b60
AchmadAndhika <user.email>
2024-12-09 22:24:15 +0700

    dika

---
+++ home.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+    <link rel="stylesheet" href="style.css?v=<?= filemtime('style.css')?>">
+</head>
+
+<body>
+    <div class="container">
+        <nav>
+            <a href="home.php">Home</a>
+            <a href="dataBarang.php">Data Barang</a>
+            <a href="dataCustomer.php">Data Customer</a>
+        </nav>
+
+        <h1>APLIKASI PENGINPUTAN DATA</h1>
+
+        <p>Aplikasi ini dirancang untuk mengelola produk dan pelanggan Anda secara efisien</p>
+
+        <ul>
+            <li>Data Barang: Melihat, menambah, dan menghapus informasi produk barang</li>
+            <li>Data Customer: Melihat, menambah, dan menghapus informasi pelanggan.</li>
+        </ul>
+
+    </div>
+</body>
+
+</html>
